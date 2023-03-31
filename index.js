d53c14caeb24458967477807923087445e09cd41
Mauricio Souza da Silva <mauriciiosouza51@gmail.com>
2023-03-30 23:03:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var listafilmes = [
+    "https://br.web.img3.acsta.net/pictures/17/07/20/15/53/052550.jpg",
+    "https://programadoresbrasil.com.br/wp-content/uploads/2020/04/privacidade-hackeada-768x1024.jpg",
+    "https://uploads.jovemnerd.com.br/wp-content/uploads/2017/12/DY8AYCeU8AAKguH.jpg-orig.jpg",
+    "https://cinearaujo.com.br/filmes/filme3232_cp.jpg",
+    "https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg"
+]
+
+
+for(var i=0; i < listafilmes.length; i++){
+    document.write("<img src=" + listafilmes[i] + ">");
+} 
